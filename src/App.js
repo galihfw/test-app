856ac5f5cc205acd8b44e78edefc359f0bd44840
DESKTOP-NGISFV2\galih <galihfw23@gmail.com>
2022-12-29 16:49:37 +0700

    TEST-mitrasolutech

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Table from 'react-bootstrap/Table';
 
-function App() {
+export default function App() {
+
+  const [ price, setPrice ] = useState(12500);
+
+  // Triggered when the value gets updated while scrolling the slider:
+  const handleInput = (e)=>{
+    setPrice( e.target.value );
+  }
+  const makanan = [
+    { id: 1,name: "BAKSO", price: 12000  },
+    { id: 2,name: "MIE AYAM", price: 15000  },
+    { id: 3,name: "AYAM BAKAR", price: 13000  }
+  ];
+  const result = makanan.reduce((total, currentValue) => total = total + currentValue.price,0);
+
+console.log(result);  //total
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+      <Table striped bordered hover variant="dark">
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Nama Makanan</th>
+          <th>Harga</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>1</td>
+          <td>Bakso</td>
+          <td>12000</td>
+        </tr>
+        <tr>
+          <td>2</td>
+          <td>Ayam Bakar</td>
+          <td>15000</td>
+        </tr>
+        <tr>
+          <td>3</td>
+          <td>Mie Ayam</td>
+          <td>13000</td>
+        </tr>
+      </tbody>
+    </Table>
+      </div>
+      <input type="range" onInput={ handleInput } />
+      <h1>Price: { price }</h1>
+      <div>
+      { makanan.filter( mkn => { return mkn.price > parseInt(price, 10) }).map( mkn => {
+        return <p key={mkn.id}>{ mkn.id } |{mkn.name}| { mkn.price }; </p>
+      })}        
+      </div>
+      <p>Total : {result}</p>
+      
     </div>
   );
 }
-
-export default App;
